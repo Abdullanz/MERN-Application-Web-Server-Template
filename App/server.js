16d0da57cf49acbcb8bd26e023868902a26e0912
Abdullah Najjar <15985254+Abdullanz@users.noreply.github.com>
2021-03-03 13:51:38 +0300

    configurations still

--- App/server.js
+++ App/server.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 const path = require("path")
 const PORT = 8000;
-
+const server = app.listen(PORT);
 
 //To point the server to render the build file for the frontend
 app.use(express.static(path.join(__dirname, "client","build")));
 
 //specifying which port to run the server
-app.listen(PORT, () => {
+server.on('listening', () => {
     console.log('Server is listening...');
 });
