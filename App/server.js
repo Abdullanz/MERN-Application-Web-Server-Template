052dbd072665ee5022f3b5439413ff940268b3f3
Abdullah Najjar <15985254+Abdullanz@users.noreply.github.com>
2021-03-11 19:19:00 +0300

    fixed routing, need to understand the details

--- App/server.js
+++ App/server.js
@@ -3,12 +3,33 @@ const app = express()
 const path = require("path")
 const PORT = 8000;
 const server = app.listen(PORT);
+const bodyParser = require('body-parser'); // This is used to create middlewares to handle objects
 
 //TODO: FIX THE ROUTING ISSUE FOR THE CAR COMPONENT!
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//To point the server to render the build file for the frontend
+//GET request
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+
+//POST request
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
+
+//To point the server to render the build file which only contains the static files of the frontend
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Handle React routing, return all requests to React app
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 //specifying which port to run the server
 server.on('listening', () => {
